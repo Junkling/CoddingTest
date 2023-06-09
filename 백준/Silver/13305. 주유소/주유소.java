da27500586a5c15036cc167924ec03a475960abb
Junkling <106804716+Junkling@users.noreply.github.com>
2023-06-09 20:34:26 +0900

    [Silver III] Title: 주유소, Time: 1052 ms, Memory: 171820 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] km = new int[n - 1];
+        int[] price = new int[n];
+        for (int i = 0; i < n - 1; i++) {
+            km[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            price[i] = sc.nextInt();
+        }
+        int index = 0;
+        int prev = Integer.MAX_VALUE;
+        int sum = 0;
+        int location = 0;
+        while (location<n-1) {
+            for (int i = index; i < n-1; i++) {
+                if(price[i]<=prev){
+                    index = i;
+                    sum += price[index]*km[i];
+                    prev = price[index];
+                    location++;
+                }else{
+                    sum += prev*km[i];
+                    index = i;
+                    location++;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
