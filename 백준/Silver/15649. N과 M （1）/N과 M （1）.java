235b57c3c64b861b4a22353b9a5a8572753fade1
Junkling <106804716+Junkling@users.noreply.github.com>
2023-07-03 21:25:27 +0900

    [Silver III] Title: N과 M (1), Time: 1072 ms, Memory: 48660 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,40 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    static int[] arr;
+    static int M;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        M = sc.nextInt();
+        arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = i + 1;
+        }
+        for (int i = 0; i < N; i++) {
+            Queue<Integer> q = new LinkedList<>();
+            boolean[] visit= new boolean[N];
+            q.offer(arr[i]);
+            visit[i] = true;
+            String s = Integer.toString(arr[i]);
+            bfs(s, visit, 1);
+        }
+    }
+
+    static void bfs(String s, boolean[] visit, int depth) {
+        if (depth == M) {
+            System.out.println(s);
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if(visit[i]) continue;
+            String add = " "+arr[i];
+            visit[i] = true;
+            bfs(s+add, visit, depth+1);
+            visit[i] = false;
+        }
+    }
+}
